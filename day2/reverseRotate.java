bfce65bf0198943fa8bcfa5f9a73e751e9f2f55d
Antony Munene <amunene@VIKI.local>
2019-02-26 14:16:48 -0800

    alternative code for reverse && rotate

--- day2/reverseRotate.java
+++ day2/reverseRotate.java
@@ -56,3 +56,26 @@ public class RevRot {
         return rotatedStr.toString();
     }
 }
+
+//Alternative better solution
+
+class RevRot {
+
+    public static String revRot(String nums, int sz) {
+        StringBuffer groups = new StringBuffer();
+        for (int i = 0, len = nums.length(); i + sz <= len && sz > 0; i += sz) {
+            String group = nums.substring(i, i + sz);
+            groups.append(isDivisible(group) ? new StringBuffer(group).reverse() : group.substring(1) + group.charAt(0));
+        }
+        return groups.toString();
+    }
+
+    public static boolean isDivisible(String group) {
+        int sum = 0;
+        for (char num : group.toCharArray()) {
+            sum += Character.getNumericValue(num);
+        }
+        return sum % 2 == 0;
+    }
+
+}
